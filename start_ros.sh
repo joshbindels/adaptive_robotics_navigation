77cf35d07d80a915ad5b664877978dd70107c66a
joshbindels <josh.bindels@gmail.com>
2020-04-01 01:50:23 +0200

    update turtlebot starting positions

--- start_ros.sh
+++ start_ros.sh
@@ -2,7 +2,7 @@
 
 if [ $1 == "g" ] ;
 then
-	roslaunch turtlebot3_gazebo turtlebot3_maze.launch
+	roslaunch turtlebot3_gazebo turtlebot3_maze.launch x_pos:=-5.95 y_pos:=-4.44
 elif [ $1 == "r" ] ;
 then
 	echo "start rviz";
